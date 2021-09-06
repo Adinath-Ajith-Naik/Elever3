8198f0c9ef8505d4e619fb0a56700094fc25e39b
ADINATH AJITH NAIK <naik.adi2001@gmail.com>
2021-09-06 18:08:09 +0530

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './shared/layout/navigation-bar/navigation-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UploadImageComponent } from './pages/upload-image/upload-image.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { MyCollectionsComponent } from './pages/my-collections/my-collections.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    HomeComponent,
+    UploadImageComponent,
+    LoginComponent,
+    ProfileComponent,
+    MyCollectionsComponent
   ],
   imports: [
     BrowserModule,
